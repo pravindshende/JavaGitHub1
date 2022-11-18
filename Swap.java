cae032c0af4675ed6cdb19aa0f5a1118c8d26f74
pravindshende <pravindshende951@gmail.com>
2022-11-18 18:51:05 +0530

    first commit

---
+++ Swap.java
@@ -0,0 +1,18 @@
+package Practice_Programs;  
+import java.util.Scanner;
+
+public class Swap_num {  
+	
+	public static void main(String[]args) {
+		int x,y,temp;
+		System.out.println("Enter the value of X & Y");
+		Scanner in=new Scanner(System.in);
+		x=in.nextInt();
+		y=in.nextInt();
+		System.out.println("Before Swapping"+x+" "+y);
+		temp=x;
+		x=y;
+		y=temp;
+		System.out.println("After Swapping"+x+" "+y);
+	}
+}
